8e6914286d89781ba2f80dc6098eb230da9e5ada
Germinal Camps <gcampsbarjau@gmail.com>
2021-06-09 08:41:49 +0200

    Refactored Express

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const data = require("./data/pokemon.js");
 
-app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE"),
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-  res.contentType("application/json");
+  res.type("json");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE"
+  });
+
   next();
 });
 
@@ -20,22 +17,13 @@ app.get("/pokemon", function(req, res) {
 });
 
 app.get("/pokemon/:id", function(req, res) {
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    if (parseInt(data[i].id) === parseInt(req.params.id)) {
-      return res.send(data[i]);
-    }
-  }
-  res.status(404).send("Pokemon does not exist");
-
-  /*
-  const pokemon = data.find(e => parseInt(e.id) === parseInt(req.params.id))
+  const pokemon = data.find(e => +e.id === +req.params.id);
 
   if (!pokemon) {
-    res.status(404).send('Pokemon does not exist');
+    return res.status(404).send("Pokemon does not exist");
   }
+
   res.send(pokemon);
-  */
 });
 
 app.post("/pokemon", function(req, res) {
